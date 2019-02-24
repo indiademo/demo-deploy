7e8ad250485000af1ca11ba56eac2936ab432d7d
indiademo <abhishek03094@gmail.com>
2019-02-24 15:52:57 +0530

    latest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+var PORT = process.env.PORT || 8080
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/demo-deploy'));
@@ -13,4 +14,8 @@ res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+
+app.listen(PORT,function(){
+    console.log("8080")
+})
